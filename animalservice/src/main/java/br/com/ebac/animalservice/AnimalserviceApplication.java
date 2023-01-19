36982285070954d3f192219f664de06debdabdb6
MarcoJunior <mcarrarajr@uol.com.br>
2023-01-19 16:53:39 -0300

    Criação do Projeto

---
+++ animalservice/src/main/java/br/com/ebac/animalservice/AnimalserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.ebac.animalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalserviceApplication.class, args);
+	} 
+
+}
